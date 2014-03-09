3a3cf61ad2bdedbe47c410f9634ff3bec9882e75
Arko <Arko@10.0.0.51>
2014-03-10 03:02:32 +1100

    Unit Test

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -1,14 +1,32 @@
 <?php
-
 class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
-
+    
+//     public function testCanDoUnitTest() {
+//         $this->assertTrue(true);
+//     }
+    
+    
     public function setUp()
     {
-        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
-        parent::setUp();
+    	$application = new Zend_Application(
+    			APPLICATION_ENV,
+    			APPLICATION_PATH . '/configs/application.ini'
+    	);
+    
+    	$this->bootstrap = $application;
+    	return parent::setUp();
     }
-
-
+    
+    
+    
+    // Test of html
+    public function testHtmlResult()
+    {
+    	$this->dispatch('/index/index');
+    	$this->assertResponseCode(200);
+    	$this->assertQueryContentContains('h1', 'WHAT TO COOK FOR TONIGHT ?');
+    	$this->assertQuery('form#fridge_form');
+    }
 }
-
+?>
